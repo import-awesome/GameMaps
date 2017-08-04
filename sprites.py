9caa5d4905a15a1b2a4e72857e32bfb7b37a8baf
edavid33 <edavid3367@gmail.com>
2017-08-04 14:27:27 -0400

    Add files via upload

--- sprites.py
+++ sprites.py
@@ -1,15 +1,17 @@
+import pygame
+
 #surfaces to hold the sprite images
-babydragon = pygame.image.load("/home/erik/Desktop/babydragon.png")
-bug = pygame.image.load("/home/erik/Desktop/bug.png")
-elderdragon = pygame.image.load("/home/erik/Desktop/elderdragon.png")
-grapeape = pygame.image.load("/home/erik/Desktop/grapeape.png")
-madelephant = pygame.image.load("/home/erik/Desktop/madelephant.png")
-mantis = pygame.image.load("/home/erik/Desktop/mantis.png")
-phoenix = pygame.image.load("/home/erik/Desktop/phoenix.png")
-razorbear = pygame.image.load("/home/erik/Desktop/razorbear.png")
-tadpol = pygame.image.load("/home/erik/Desktop/tadpol.png")
-vixen = pygame.image.load("/home/erik/Desktop/vixen.png")
-smileycyclops = pygame.image.load("/home/erik/Desktop/smileycyclops.png")
+babydragon = pygame.image.load("babydragon.png")
+bug = pygame.image.load("bug.png")
+elderdragon = pygame.image.load("elderdragon.png")
+grapeape = pygame.image.load("grapeape.png")
+madelephant = pygame.image.load("madelephant.png")
+mantis = pygame.image.load("mantis.png")
+phoenix = pygame.image.load("phoenix.png")
+razorbear = pygame.image.load("razorbear.png")
+tadpol = pygame.image.load("tadpol.png")
+vixen = pygame.image.load("vixen.png")
+smileycyclops = pygame.image.load("smileycyclops.png")
 
 #lists that contain the sprites
 enemy_list1 = [grapeape,bug]
