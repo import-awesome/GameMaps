c530329751fcbfb8c7bcfcf49675199bf2198885
edavid33 <edavid3367@gmail.com>
2017-08-04 13:42:50 -0400

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,20 @@
+#surfaces to hold the sprite images
+babydragon = pygame.image.load("/home/erik/Desktop/babydragon.png")
+bug = pygame.image.load("/home/erik/Desktop/bug.png")
+elderdragon = pygame.image.load("/home/erik/Desktop/elderdragon.png")
+grapeape = pygame.image.load("/home/erik/Desktop/grapeape.png")
+madelephant = pygame.image.load("/home/erik/Desktop/madelephant.png")
+mantis = pygame.image.load("/home/erik/Desktop/mantis.png")
+phoenix = pygame.image.load("/home/erik/Desktop/phoenix.png")
+razorbear = pygame.image.load("/home/erik/Desktop/razorbear.png")
+tadpol = pygame.image.load("/home/erik/Desktop/tadpol.png")
+vixen = pygame.image.load("/home/erik/Desktop/vixen.png")
+smileycyclops = pygame.image.load("/home/erik/Desktop/smileycyclops.png")
+
+#lists that contain the sprites
+enemy_list1 = [grapeape,bug]
+enemy_list2 = [mantis,vixen]
+enemy_list3 = [babydragon,tadpol]
+enemy_list4 = [razorbear,smileycyclops]
+enemy_list5 = [phoenix,madelephant]
+enemy_boss = [elderdragon]
